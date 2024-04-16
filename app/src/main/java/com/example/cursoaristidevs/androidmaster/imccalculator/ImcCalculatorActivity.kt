a54ab2c00dfb3f70f3058858aa8e2979b595ccc1
VictorSantiagoFerrera <victor.santiago@the-cocktail.com>
2024-04-16 16:27:31 +0200

    Hasta 6:08:23

--- app/src/main/java/com/example/cursoaristidevs/androidmaster/imccalculator/ImcCalculatorActivity.kt
+++ app/src/main/java/com/example/cursoaristidevs/androidmaster/imccalculator/ImcCalculatorActivity.kt
@@ -1,11 +1,14 @@
 package com.example.cursoaristidevs.androidmaster.imccalculator
 
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.cardview.widget.CardView
 import androidx.core.content.ContextCompat
 import com.example.cursoaristidevs.R
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.slider.RangeSlider
 import java.text.DecimalFormat
 
@@ -13,11 +16,21 @@ class ImcCalculatorActivity : AppCompatActivity() {
 
     private var isMaleSelected: Boolean = true
     private var isFemaleSelected: Boolean = false
+    private var currentWeight: Int = 60
+    private var currentAge: Int = 20
+    private var currentHeight: Int = 120
 
     private lateinit var viewMale: CardView
     private lateinit var viewFemale: CardView
     private lateinit var tvHeight: TextView
     private lateinit var rsHeight: RangeSlider
+    private lateinit var btnSubtractWeight: FloatingActionButton
+    private lateinit var btnPlusWeight: FloatingActionButton
+    private lateinit var tvWeight: TextView
+    private lateinit var tvAge: TextView
+    private lateinit var btnPlusAge: FloatingActionButton
+    private lateinit var btnSubtractAge: FloatingActionButton
+    private lateinit var btnCalculate: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_imc_calculator)
@@ -31,6 +44,13 @@ class ImcCalculatorActivity : AppCompatActivity() {
         viewFemale = findViewById(R.id.viewFemale)
         tvHeight = findViewById(R.id.tvHeight)
         rsHeight = findViewById(R.id.rsHeight)
+        btnSubtractWeight = findViewById(R.id.btnSubtractWeight)
+        btnPlusWeight = findViewById(R.id.btnPlusWeight)
+        btnSubtractAge = findViewById(R.id.btnSubtractAge)
+        btnPlusAge = findViewById(R.id.btnPlusAge)
+        tvWeight = findViewById(R.id.tvWeight)
+        tvAge = findViewById(R.id.tvAge)
+        btnCalculate = findViewById(R.id.btnCalculate)
     }
 
     private fun initListeners() {
@@ -44,9 +64,47 @@ class ImcCalculatorActivity : AppCompatActivity() {
         }
         rsHeight.addOnChangeListener { _, value, _ ->
             val df = DecimalFormat("#.##")
-            val result = df.format(value)
-            tvHeight.text = "$result cm"
+            currentHeight = df.format(value).toInt()
+            tvHeight.text = "$currentHeight cm"
         }
+
+        btnPlusWeight.setOnClickListener {
+            currentWeight += 1
+            setWeight()
+        }
+        btnSubtractWeight.setOnClickListener {
+            currentWeight -= 1
+            setWeight()
+        }
+
+        btnPlusAge.setOnClickListener {
+            currentAge += 1
+            setAge()
+        }
+
+        btnSubtractAge.setOnClickListener {
+            currentAge -= 1
+            setAge()
+        }
+
+        btnCalculate.setOnClickListener {
+            calculateImc()
+        }
+    }
+
+    private fun calculateImc() {
+        val df = DecimalFormat("#.##")
+        val imc = currentWeight / (currentHeight.toDouble() / 100 * currentHeight.toDouble() / 100)
+        val result = df.format(imc)
+        Log.i("tutorial", "el IMC es $result")
+    }
+
+    private fun setAge() {
+        tvAge.text = currentAge.toString()
+    }
+
+    private fun setWeight() {
+        tvWeight.text = currentWeight.toString()
     }
 
     private fun changeGender() {
@@ -69,5 +127,7 @@ class ImcCalculatorActivity : AppCompatActivity() {
 
     private fun initUi() {
         setGenderColor()
+        setWeight()
+        setAge()
     }
 }
