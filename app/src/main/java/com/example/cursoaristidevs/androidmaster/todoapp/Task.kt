e8b0a06381e9fcbd0e498222df82c079938150b6
VictorSantiagoFerrera <victor.santiago@the-cocktail.com>
2024-06-25 09:20:08 +0200

    Seguir en el 7:53:00

--- app/src/main/java/com/example/cursoaristidevs/androidmaster/todoapp/Task.kt
+++ app/src/main/java/com/example/cursoaristidevs/androidmaster/todoapp/Task.kt
@@ -1,3 +1,6 @@
 package com.example.cursoaristidevs.androidmaster.todoapp
 
-data class Task(val name: String, val category: TaskCategory, var isSelected: Boolean = false)
+data class Task(
+    val name: String,
+    val category: TaskCategory,
+    var isSelected: Boolean = false)
