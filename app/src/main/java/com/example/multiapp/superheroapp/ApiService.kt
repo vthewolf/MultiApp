10df5f83b12b6c55e1995f552aedb2ad48c81a71
VictorSantiagoFerrera <victor.santiago@the-cocktail.com>
2024-10-25 09:37:35 +0200

    feat: finish settings layout

--- app/src/main/java/com/example/multiapp/superheroapp/ApiService.kt
+++ app/src/main/java/com/example/multiapp/superheroapp/ApiService.kt
@@ -8,5 +8,5 @@ interface ApiService {
     @GET("search/{name}")
     suspend fun getSuperheroes(@Path("name") superheroName: String): Response<SuperHeroResponse>
     @GET("{id}")
-    suspend fun getSuperheroeDetail(@Path("id")superHeroId: String): Response<com.example.multiapp.superheroapp.SuperheroDetailResponse>
+    suspend fun getSuperheroeDetail(@Path("id")superHeroId: String): Response<SuperheroDetailResponse>
 }
